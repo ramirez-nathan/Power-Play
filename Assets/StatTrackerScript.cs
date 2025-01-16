4842e10118253365ea165eb5f1371b555b66a1fe
Ampherost <Ampherost235@gmail.com>
2025-01-16 15:46:37 -0800

    added scene for tracking stats

--- Assets/StatTrackerScript.cs
+++ Assets/StatTrackerScript.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class StatTrackerScript : MonoBehaviour
 {
+    // Components and References
+    public PlayerMain player;               // Reference to the player for retrieving stats
+    // public GameObject stage;             // Reference to the stage GameObject (for ground checks).
+    // private SpriteRenderer spriteRenderer; // SpriteRenderer for changing player sprites.
+    // public AudioSource deathSound;       // A sound that gets played when the character gets destroyed
+    // public Collider2D attackCollider;    // The collider representing the player's attack hitbox
+    // public enemyScript enemyScwipt;      // Reference to enemy code
+    // public gameOverScreen gameOverScween; // The game over screen
+
+
     // Start is called before the first frame update
     void Start()
     {
